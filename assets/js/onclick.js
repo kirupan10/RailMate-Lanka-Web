a1f120c757527edd3a45a4671f021b03ee0d7470
kirupan10 <ikirupan@gmail.com>
2023-03-09 22:12:03 +0530

    profile page

--- assets/js/onclick.js
+++ assets/js/onclick.js
@@ -6,3 +6,12 @@ document.getElementById("logout").onclick = function () {
 document.getElementById("ViewRecords").onclick = function () {
     location.href = "view_record.html";
 };
+
+document.getElementById("loginButton").onclick = function () {
+    location.href = "profile.html";
+};
+
+document.getElementById("signupButton").onclick = function () {
+    location.href = "profile.html";
+};
+
