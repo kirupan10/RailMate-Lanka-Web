fe1729c0c18d259cb052abdb3d624d9396cb8a5e
MOHAMED SHUKRY <mhdshukry110@gmail.com>
2023-06-10 14:42:54 +0530

    js update
    js updated

---
+++ assets/js/onclick.js
@@ -0,0 +1,11 @@
+document.getElementById("logout").onclick = function () {
+    alert("Logging out...");
+    location.href = "index.html";
+};
+document.getElementById("ViewBooking").onclick = function () {
+    location.href = "ReservationHistory.html";
+};
+
+
+
+
